2f48544c3d5b2b1fb4caaeee05674c5df615243d
Julian Stecklina <js@alien8.de>
2020-07-12 23:30:01 +0200

    studio-link: v20.03.8 -> v20.05.0

--- nix/build.nix
+++ nix/build.nix
@@ -2,14 +2,14 @@
 
 stdenv.mkDerivation rec {
   pname = "studio-link";
-  version = "v20.03.8";
+  version = "v20.05.0";
 
   src = fetchurl {
-    url = "https://download.studio.link/releases/v20.03.8-stable/linux/studio-link-standalone-v20.03.8.tar.gz";
-    sha256 = "1xwnai3nz6larz4c99zr1s76ayhya6yxic3bdwa84p41vyb6rrdg";
+    url = "https://download.studio.link/releases/${version}-stable/linux/studio-link-standalone-${version}.tar.gz";
+    sha256 = "1x0ihasmvc1dbl5n7q8v1rzzk8fd0s60pk5av49fk03pymdlf4yq";
   };
 
-  buildInputs = [ zlib libpulseaudio gcc-unwrapped alsaLib ];
+  buildInputs = [ zlib libpulseaudio alsaLib ];
   nativeBuildInputs = [ autoPatchelfHook ];
 
   # The tarball doesn't contain a directory to change into.
